47aba0b514aa8d99af96fdf6cdec7e9d48bb2235
Olivia Gilbert <umhello2020@gmail.com>
2023-03-21 20:46:04 -0500

    fixed module.exports and got server to run successfully added nodemon into scripts and changed main from index to server.js

--- Recipe app/config/connection.js
+++ Recipe app/config/connection.js
@@ -8,4 +8,4 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     port: 3306
 });
 
-modules.export = sequelize;
+module.exports = sequelize;
--- Recipe app/server.js
+++ Recipe app/server.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const express = require('express');
+// const path = require('path');
+// const express = require('express');
 const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars');
+// const exphbs = require('express-handlebars');
 require('dotenv').config();
-const hbs = exphbs.create({});
-const app = express();
-const PORT = process.env.PORT || 3001;
+// const hbs = exphbs.create({});
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
 sequelize.sync()
     .then(() => {
