14c609363dbef1d474e704a225791e904a43d657
KarimB101 <karim.baptiste@yahoo.com>
2023-03-28 15:21:32 -0500

    added tables to schema

--- Recipe app/db/schema.sql
+++ Recipe app/db/schema.sql
@@ -1,2 +1,37 @@
+-- Step 1: Create the recipe_db database (if it doesn't exist)
 DROP DATABASE IF EXISTS recipe_db;
 CREATE DATABASE recipe_db;
+
+USE recipe_db;
+
+-- Step 2: Create the users table
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Step 3: Create the recipes table
+CREATE TABLE recipes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    ingredients TEXT NOT NULL,
+    instructions TEXT NOT NULL,
+    user_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- Step 4: Create the comments table
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    user_id INT,
+    recipe_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id)
+);
