396cf169fb627d5238068d06aa1f22bf1579b562
KarimB101 <karim.baptiste@yahoo.com>
2023-03-28 18:53:49 -0500

    everything updated

--- Recipe app/config/connection.js
+++ Recipe app/config/connection.js
@@ -1,8 +1,14 @@
 const Sequelize = require('sequelize');
+const mysql = require('mysql2');
+const express = require('express')
 require('dotenv').config();
 
 // create connection to our database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PW, 
+    {
     host: "localhost",
     dialect: "mysql",
     port: 3306
