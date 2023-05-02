eb817013f870d0c5421d3eec81775d11060119c8
Olivia Gilbert <umhello2020@gmail.com>
2023-05-02 16:14:09 -0500

    worked on views

--- public/js/login.js
+++ public/js/login.js
@@ -31,7 +31,7 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-signup').value.trim();
   
     if (username && email && password) {
-      const response = await fetch('/signup', {
+      const response = await fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
