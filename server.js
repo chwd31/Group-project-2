f5cb9561b949eecd1849a8a10bb80e4c8eb43da8
Olivia Gilbert <umhello2020@gmail.com>
2023-05-02 22:04:39 -0500

    fixed main handlebars issue

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const sess = {
 app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'hbs');
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
